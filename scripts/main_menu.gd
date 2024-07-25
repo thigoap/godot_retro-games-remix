8393d35bf86d829523bb205a7fd7399ea877865a
thigoap <appthigo@gmail.com>
2024-07-25 01:29:04 -0300

    Created statistics page and adjusted visual.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,12 +4,11 @@ extends Control
 func _on_tic_tac_toe_pressed():
 	get_tree().change_scene_to_file("res://scenes/tic tac toe/tic_tac_toe.tscn")
 
-
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
-
+func _on_statistics_pressed():
+	get_tree().change_scene_to_file("res://scenes/statistics.tscn")
+	
 func _on_quit_pressed():
 	get_tree().quit()
-
-
